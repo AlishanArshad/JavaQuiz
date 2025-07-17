2bd57494b097c17ac100a41f4fcbab29d768722f
Alishan Arshad <aa9641434@gmail.com>
2025-07-17 15:19:55 +0500

    print input

--- src/Quiz/QuestionService.java
+++ src/Quiz/QuestionService.java
@@ -1,7 +1,10 @@
 package Quiz;
 
+import java.util.Scanner;
+
 public class QuestionService {
     Question[] questions = new Question[3];
+    String[] selection = new String[3];
 
     public QuestionService(){
         questions[0] = new Question(1,"what comes after 2", "1", "2", "3", "4", "3");
@@ -9,9 +12,23 @@ public class QuestionService {
         questions[2] = new Question(1,"what is the largest among all", "1", "2", "3", "4", "4");
     }
 
-    public void displayQuestions(){
+    public void playQuiz(){
+        int i = 0;
         for(Question q : questions){
-            System.out.println(q);
+            System.out.println(q.getId());
+            System.out.println(q.getQuestion());
+            System.out.println(q.getOpt1());
+            System.out.println(q.getOpt2());
+            System.out.println(q.getOpt3());
+            System.out.println(q.getOpt4());
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter Answer : ");
+            selection[i] = input.nextLine();
+            i++;
+        }
+        for(String s : selection){
+            System.out.println(s);
+
         }
 
     }
